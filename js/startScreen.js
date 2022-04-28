6c8c36e403f148746574b1e6f4bf450aa1d912be
Álex <al404395@uji.es>
2022-04-28 18:29:10 +0200

    startScreen complete structure
    The whole definition of the startScreen state is structured: load the assets and add them in the game area.

--- js/startScreen.js
+++ js/startScreen.js
@@ -1,3 +1,5 @@
+const BUTTON_OFFSET_Y = 100; // Length of the gap between the buttons in the state
+
 let startScreen = {
     preload: loadStartScreen,
     create: createStartScreen,
@@ -5,45 +7,83 @@ let startScreen = {
 //Variable definitions: assets (sounds, sprites, etc)
 
 function loadStartScreen() {
-    //load assets with game.load.asset(parameter1, parameter2, etc)
-    game.load.spritesheet("fly", "./assets/imgs/fly.png");
-    // load the elements that will work as the buttons to change to other states
+    game.load.image('bg', '../assets/imgs/background.png')
+    game.load.spritesheet("fly", "../assets/imgs/fly.png");
+    // Load the images corresponding to the buttons
+    // the following route names could change
+    game.load.image("stageA", "../assets/imgs/stageA.png");
+    game.load.image("stageB", "../assets/imgs/stageB.png");
+    game.load.image("stageC", "../assets/imgs/stageC.png");
+    game.load.image("about", "../assets/imgs/about.png");
 }
 
 function createStartScreen() {
     //initialise assets on the game screen: set their locations, scales, rotations, tweens, etc
-    // Create the tween that makes the fly move around the borders of the game area
-    let bouncingFly = game.add.sprite(0, GAME_AREA_HEIGHT /* - altura del sprite*/, "fly"
+
+    // Add the background image to the screen
+    let BG = game.add.image(0, 0, "bg");
+    BG.scale.setTo(1.8); //will be modified in the future
+
+    // Add the game title at the top
+    let gameTitle = "FROG TYPING GAME";
+    let style = {
+        font: 'Open Sans',
+        fontSize: "35px",
+    }
+    game.add.text(0, 0, gameTitle, style); //modify the title aligment
+
+    // Position the buttons in the game area
+    let btnStageA = game.add.button(GAME_AREA_WIDTH / 2, GAME_AREA_HEIGHT / 2,
+        "stageA", goToStageA);
+    btnStageA.anchor.setTo(0.5, 0.5);
+    //scale?
+    let btnStageB = game.add.button(GAME_AREA_WIDTH / 2, GAME_AREA_HEIGHT / 2 + BUTTON_OFFSET_Y,
+        "stageB", goToStageB);
+    btnStageB.anchor.setTo(0.5, 0.5);
+    //scale?
+    let btnStageC = game.add.button(GAME_AREA_WIDTH / 2, GAME_AREA_HEIGHT / 2 + 2 * BUTTON_OFFSET_Y,
+        "stageC", goToStageC);
+    btnStageC.anchor.setTo(0.5, 0.5);
+    //scale?
+
+    let btnAbout = game.add.button(GAME_AREA_WIDTH / 2, GAME_AREA_HEIGHT / 2 + 3.5 * BUTTON_OFFSET_Y,
+        "about", goToAboutScreen);
+    btnAbout.anchor.setTo(0.5, 0.5);
+    //scale?
+
+    // Fly tween: a fly just chilling around the game area borders
+    let bouncingFly = game.add.sprite(0 + SPRITE_FLY_WIDTH / 2,
+        GAME_AREA_HEIGHT - SPRITE_FLY_HEIGHT, "fly"
         /*, índice que queremos cargar si tenemos una sábana de sprites*/);
     bouncingFly.anchor.setTo(0.5, 0.5);
+    bouncingFly.scale.setTo(0.1); // will be deleted soon due to the assets resizing
 
     let flyTween = game.add.tween(bouncingFly).to({
-            y: 0
+            y: SPRITE_FLY_HEIGHT / 2
         }, 4500, Phaser.Easing.Linear.None)
         .to({
             angle: 90
         }, 250, Phaser.Easing.Linear.None)
         .to({
-            x: GAME_AREA_WIDTH /* - altura del sprite */
+            x: GAME_AREA_WIDTH - SPRITE_FLY_HEIGHT / 2
         }, 4500, Phaser.Easing.Linear.None)
         .to({
-            angle: 90
+            angle: 180
         }, 250, Phaser.Easing.Linear.None)
         .to({
-            y: GAME_AREA_HEIGHT
+            y: GAME_AREA_HEIGHT - SPRITE_FLY_HEIGHT / 2
         }, 4500, Phaser.Easing.Linear.None)
         .to({
-            angle: 90
+            angle: 270
         }, 250, Phaser.Easing.Linear.None)
         .to({
-            x:0
+            x: SPRITE_FLY_HEIGHT / 2
         }, 4500, Phaser.Easing.Linear.None)
         .to({
-            angle: 90
+            angle: 360
         }, 250, Phaser.Easing.Linear.None);
     flyTween.loop(true);
     flyTween.start();
-
 }
 
 function goToAboutScreen() {
@@ -51,9 +91,13 @@ function goToAboutScreen() {
 }
 
 function goToStageA() {
-    game.state.start("stageA");
+    // game.state.start("stageA");
 }
 
 function goToStageB() {
     // game.state.start("stageB");
+}
+
+function goToStageC() {
+    // game.state.start("stageC");
 }
