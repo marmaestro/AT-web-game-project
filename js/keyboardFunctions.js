2da167e21603d607a8569407ce0402badc235e72
Marta Maestro <al404230@uji.es>
2022-05-03 14:18:11 +0200

    Conteo de aciertos y errores añadido.
    Estructura de las funciones planteadas, queda ver cómo será el texto en los OWPs para poder tratar bien la "eliminación" de palabras.

--- js/keyboardFunctions.js
+++ js/keyboardFunctions.js
@@ -1,26 +1,42 @@
 let activeOWP;
+let typed = 0;
+let correct = 0;
+
 
 function readKeyboard(e) {
     if(e.keyCode >= Phaser.Keyboard.A
     && e.keyCode <= Phaser.Keyboard.Z) {
         checkLetter(e);
+        typed++;
     }
 }
 
 function checkLetter(e) {
-    if(e == activeOWP );
-}
-
-function shakeLetters() {
-    for (const child of letters.children) {
-        child.x += Math.random() * 2 - 1;
-        child.y += Math.random() * 2 - 1;
-        child.angle += Math.random() * 10 - 5;
+    if (isNextLetterInOWP(e)) { //check if the letter typed is the correct one
+        deactivateLetter(); //deactivate said letter if true
+        correct++;
     }
 }
 
-function processLetter(item, pointer) {
+/*function processLetter(item, pointer) {
     item.destroy(); // frees up memory
     // kill() removes it from display list,
     // but not from the group
+}*/
+
+function isNextLetterInOWP(e) {
+
+    return true;
+}
+
+function deactivateLetter() { //deactivates the correctly-typed letter in the OWP
+
+}
+
+
+
+
+
+function calculateAccuracy() {
+    return (correct / typed) * 100;
 }
