c059e7b2675568e067df63e08b4e0ccf88f47d35
Marta Maestro <al404230@uji.es>
2022-05-09 15:33:48 +0200

    Some progress in the creation and update states of stage A.

--- js/stageA.js
+++ js/stageA.js
@@ -9,12 +9,26 @@ function loadStageA() {
 }
 
 function createStageA() {
+    // background
+    game.add.image(0, 0, 'bg').setOrigin(0, 0);
+
+    createOWPs(nOWPs);
     createSounds();
     createExplosions(EXPLOSIONS_GROUP_SIZE);
+
+    // arcade phisics and collisions
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    game.physics.arcade.enable([typist, owps]);
+    typist.body.onCollide = new Phaser.Signal();
+    typist.body.onCollide.add(collision, this);
 }
 
 function updateStageA() {
+    game.physics.arcade.collide(typist, owps);
+}
 
+function collision() {
+    goToHUDScreen();
 }
 
 function goToHUDScreen() {
